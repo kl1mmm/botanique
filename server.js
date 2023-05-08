9c22ecba283175acac96a69457c755997d8dff3f
kl1mmm <klimovilya07@mail.ru>
2023-05-08 18:26:31 +0300

    some upd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path')
 const morgan = require('morgan')
 
+// const equipData = require('models/equipment')
 const app = express();
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
@@ -9,45 +10,46 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const PORT = 8080;
-
 const createPath = (page) => path.resolve(__dirname, 'public/ejs-views', `${page}.ejs`);
+
+const equipmentData = [
+    {
+        id: 1,
+        name: 'pH-метр Mettler-Toledo International, Inc. SevenCompact S220',
+        img: '../images/equip/pH.svg',
+        status: 'Свободен'
+    },
+    {
+        id: 2,
+        name: ' Спектрофотометр Varian, Inc Cary 50 Bio',
+        img: '../images/equip/spectr.svg',
+        status: 'Свободен'
+    },
+    {
+        id: 3,
+        name: 'Титратор',
+        img: '../images/equip/titrator.svg',
+        status: 'Свободен'
+    },
+    {
+        id: 4,
+        name: 'Коагулометр Tcoag, KC 4 Delta',
+        img: '../images/equip/coag.svg',
+        status: 'Свободен'
+    }]
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    const equipment = [
-        {
-            id: 1,
-            name: 'pH-метр Mettler-Toledo International, Inc. SevenCompact S220',
-            img: '../images/equip/pH.svg',
-            status: 'Свободен'
-        },
-        {
-            id: 2,
-            name: ' Спектрофотометр Varian, Inc Cary 50 Bio',
-            img: '../images/equip/spectr.svg',
-            status: 'Свободен'
-        },
-        {
-            id: 3,
-            name: 'Титратор',
-            img: '../images/equip/titrator.svg',
-            status: 'Свободен'
-        },
-        {
-            id: 4,
-            name: 'Коагулометр Tcoag, KC 4 Delta',
-            img: '../images/equip/coag.svg',
-            status: 'Свободен'
-        }
-    ];
-    res.render(createPath('main'), {equipment})
+    const equip = equipmentData;
+    res.render(createPath('main'), {equip})
 })
 
-app.get('/analytics/', (req, res) => {
-    res.render(createPath('analytics'))
+app.get('/analytics/:id', (req, res) => {
+    const equip = equipmentData[req.params.id];
+    res.render(createPath('analytics'), {equip})
 })
 
 app.use((req, res) => {
